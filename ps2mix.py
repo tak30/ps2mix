40489b4fa7be4d7dafdefc01013579f83e599546
unknown <plopezvi@LCG-8P4BK12.usersad.everis.int>
2015-04-28 19:25:11 +0200

    Fixed problem with utf8 and added new feature

--- ps2mix.py
+++ ps2mix.py
@@ -15,7 +15,10 @@ def parse_args():
                         help='Usage: -c file|directory')
     args = parser.parse_args()
     if args.csv is not None:
-        migrate_csvs(args.csv)
+        excluded_files_path = os.path.abspath('excluded_files.txt')
+        with open(excluded_files_path, 'rb') as file:
+            excluded_files = file.read().splitlines()
+            migrate_csvs(args.csv, excluded_files)
     else:
         parser.print_help()
         sys.exit(1)
@@ -33,7 +36,7 @@ def date_to_informix(date_text):
             return None
 
 
-def get_informix_row(ps_row, row_number):
+def get_informix_row(ps_row, row_number, add_id):
     pos = 0
     for item in ps_row:
         if item.lower() == 'true':
@@ -47,50 +50,63 @@ def get_informix_row(ps_row, row_number):
             if date is not None:
                 ps_row[pos] = date
         pos += 1
-    return [row_number] + ps_row
+    if add_id:
+        new_row = [row_number] + ps_row
+    else:
+        new_row = ps_row
+    return new_row
 
 
 def get_informix_header(ps_header, file_name):
     table_name, extension = os.path.splitext(file_name)
     column = table_name[4:] + "_id"
-    return [column] + ps_header
+    add_id = column not in unicode(ps_header)
+    if add_id:
+        new_row = [column] + ps_header
+    else:
+        new_row = ps_header
+    return new_row, add_id
 
 
-def ps_to_mix_csv(src_path, dst_path):
+def ps_to_mix_csv(src_path, dst_path, excluded_files):
     row_number = 0
     file_name = ntpath.basename(src_path)
+    add_id = file_name not in excluded_files
     with open(dst_path, "wb") as write_file:
         writer = csv.writer(write_file, delimiter=';')
         with open(src_path, "rb") as read_file:
             reader = csv.reader(read_file, delimiter=';')
             for row in reader:
                 if row_number == 0:
-                    new_row = get_informix_header(row, file_name)
+                    if (add_id):
+                        new_row, add_id = get_informix_header(row, file_name)
+                    else:
+                        new_row = row
                 else:
-                    new_row = get_informix_row(row, row_number)
+                    new_row = get_informix_row(row, row_number, add_id)
                 writer.writerow(new_row)
                 row_number += 1
 
 
-def convert_csv(path):
+def convert_csv(path, excluded_files):
     dir_path = os.path.dirname(path)
     new_dir = dir_path + "/" + 'informix'
     if os.path.isfile(new_dir):
-        print 'Error: Existence of an informix file'
+        print 'Error: Existence of a file named informix'
         sys.exit(1)
     if not os.path.exists(new_dir):
             os.makedirs(new_dir)
     new_file = new_dir + "/" + ntpath.basename(path)
-    ps_to_mix_csv(path, new_file)
+    ps_to_mix_csv(path, new_file, excluded_files)
 
 
-def migrate_csvs(path):
+def migrate_csvs(path, excluded_files):
     abs_path = os.path.abspath(path)
     if os.path.isfile(abs_path):
-        convert_csv(abs_path)
+        convert_csv(abs_path, excluded_files)
     elif os.path.isdir(abs_path):
         for each_file in glob.glob(abs_path + "/*.csv"):
-            convert_csv(each_file)
+            convert_csv(each_file, excluded_files)
 
 
 if __name__ == '__main__':
